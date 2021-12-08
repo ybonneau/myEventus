311b1041f380f3326dae2a696daffe07962bfac6
Pierre <pgeslain38@gmail.com>
2021-12-08 17:27:55 +0100

    Footer done

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import Navbar from './components/menu/navBar';
 import FilterContent from './components/home/filterContent';
+import Footer from "./components/menu/footer";
 
 export default function App() {
   return (
     <div>
       <Navbar />
       <FilterContent />
+      <Footer/>
     </div>
   );
 }
